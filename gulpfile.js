9123e8f6edb4e32d7c816970f4216f45738910b6
Merve Erdol <merve-uygun@windowslive.com>
2017-07-20 14:52:24 +0300

    till css architecture lesson2

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssimport = require('postcss-import');
+
+gulp.task('html', function(){
+  console.log("HTML CHANGED");
+});
+
+gulp.task('styles', function(){
+return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function(){
+    gulp.start('html');
+    watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  });
+});
+});
